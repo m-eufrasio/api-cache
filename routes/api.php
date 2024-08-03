4403d49504b82d49d15d619a7358de43abc13146
Matheus Eufrasio <you@example.com>
2024-08-03 20:04:15 -0300

    feat: add cache into routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Cache;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/index', function () {
+    $value = Cache::get('test_key');
+    
+    return response()->json(['value' => $value]);
+});
+
+Route::post('/store', function (Request $request) {
+    $value = Cache::put('two', $request, 3600);
+    
+    return response()->json(['value' => $value]);
 });
