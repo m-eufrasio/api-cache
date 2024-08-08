e0ac2304151645813babc44a33b4e7de4dc448c1
Matheus Eufrasio <you@example.com>
2024-08-08 19:28:20 -0300

    feat: add store and show routes

--- routes/api.php
+++ routes/api.php
@@ -3,26 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Cache;
+use App\Http\Controllers\PostController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::get('/index', [PostController::class, 'index']);
 
-Route::get('/index', function () {
-    $value = Cache::get('test_key');
-    
-    return response()->json(['value' => $value]);
-});
+Route::post('/store', [PostController::class, 'store']);
 
-Route::post('/store', function (Request $request) {
-    $value = Cache::put('two', $request, 3600);
-    
-    return response()->json(['value' => $value]);
-});
+Route::get('/show/{id}', [PostController::class, 'show']);
